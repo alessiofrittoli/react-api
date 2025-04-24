ceaa67e03f3356c32ffaf9769fdff757c66060ff
Alessio Frittoli <info@alessiofrittoli.it>
2025-04-24 19:37:46 +0200

    docs: add utility functions API Reference

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,14 @@ import { isValidElement } from 'react'
 /**
  * Check if the given `input` is a React ComponentType.
  * 
+ * @template P The props the component accepts.
+ * 
  * @param input The input to check.
  * @returns `true` if the given input is a React ComponentType, `false` otherwise.
  */
 export const isComponentType = <
-	T = unknown
->( input: unknown ): input is React.ComponentType<T> => (
+	P = unknown
+>( input: unknown ): input is React.ComponentType<P> => (
 	typeof input === 'function'
 )
 
